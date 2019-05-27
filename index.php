6e4ddb946c26fd2a0efed3bfd1c5b38daae67048
Arjan de Ruijter <rra.am1x.ionic.blok4@gmail.com>
2019-05-27 09:36:55 +0200

    form copied from BS

--- index.php
+++ index.php
@@ -13,7 +13,65 @@
   <body>
     <main>
       <section class="container-fluid"><h1>BMI CALCULATOR</h1></section>
-      <section class="container"></section>
+      <section class="container">
+        <div class="row">
+          <div class="col-4"></div>
+          <div class="col-4">
+            <form action="" method="post">
+              <div class="form-row">
+                <div class="form-group col-md-4">
+                  <label for="firstname">Voornaam</label>
+                  <input type="text" class="form-control" id="firstname" placeholder="">
+                </div>
+                <div class="form-group col-md-4">
+                  <label for="infix">tussenvoegsel</label>
+                  <input type="text" class="form-control" id="infix" placeholder=""">
+                </div>
+                <div class="form-group col-md-4">
+                  <label for="lastname">Achternaam</label>
+                  <input type="text" class="form-control" id="lastname" placeholder="">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputAddress">Address</label>
+                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+              </div>
+              <div class="form-group">
+                <label for="inputAddress2">Address 2</label>
+                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+              </div>
+              <div class="form-row">
+                <div class="form-group col-md-6">
+                  <label for="inputCity">City</label>
+                  <input type="text" class="form-control" id="inputCity">
+                </div>
+                <div class="form-group col-md-4">
+                  <label for="inputState">State</label>
+                  <select id="inputState" class="form-control">
+                    <option selected>Choose...</option>
+                    <option>...</option>
+                  </select>
+                </div>
+                <div class="form-group col-md-2">
+                  <label for="inputZip">Zip</label>
+                  <input type="text" class="form-control" id="inputZip">
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" id="gridCheck">
+                  <label class="form-check-label" for="gridCheck">
+                    Check me out
+                  </label>
+                </div>
+              </div>
+              <button type="submit" class="btn btn-primary">Sign in</button>
+            </form>
+          </div>
+          <div class="col-4"></div>
+        </div>
+      
+      </section>
       <section class="container"></section>
 
     </main>
